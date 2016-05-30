cfec1c545a4690b3acc45c6a8a738aa1d460f553
sjtryba <sjtryba@gmail.com>
2016-05-29 22:45:08 -0700

    Alarm class update
    Added 'update' function to the class

--- code/alarms.py
+++ code/alarms.py
@@ -2,23 +2,32 @@ import pygame
 
 
 class Alarm:
-    def __init__(self, tier, trigger_probability, damage_constant, resolution, sound_file_name, activated=False, silence=False):
+    def __init__(self, tier, trigger_probability, damage_constant, resolution, sound_file_name, activate=False, silence=False):
         self.tier = tier
         self.trigger_probability = trigger_probability
         self.damage_constant = damage_constant
         self.resolution = resolution
         self.sound = pygame.mixer.Sound(sound_file_name)
-        self.activated = activated
+        self.activate = activate
         self.silence = silence
 
     def trigger(self):
         # Trigger the alarm
-        self.activated = True
+        self.activate = True
 
         # If the alarm is not silenced, play the alarm sound
         if self.silence is False:
             self.sound.play()
 
     def mute(self):
-        # Silence the alarm
+        # Mute the alarm
         self.silence = True
+
+    def update(self):
+        if self.activate:
+            if pygame.mixer.get_busy():
+                if self.silence:
+                    pygame.mixer.stop()
+            else:
+                if self.silence is False:
+                    self.sound.play()
