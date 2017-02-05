23fcd719cc861079fdd5a5506c9cb0477135877b
Spencer Tryba <sjtryba@gmail.com>
2017-02-05 11:12:28 -0800

    S7SDisplay Hardware Drafting
    Created a 3D model of the S7SDisplay. Also, removed the older .STL
    version. Removed the outdated resistor puzzle panel parts and
    assemblies. Initial upload of rocket_simulator.ino

---
+++ code/rocket_simulator/rocket_simulator.ino
@@ -0,0 +1,13 @@
+# include <Wire.h>
+# include <S7SDisplay.h>
+
+void setup() {
+  Wire.begin(); // Initialize hardware I2C pins
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
